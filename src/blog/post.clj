0188fef83766be01c1690f89c0a3b73e92be1b1b
Josep M. Bach <josep.m.bach@gmail.com>
2014-07-18 15:32:07 +0200

    Show and link authors on posts

--- src/blog/post.clj
+++ src/blog/post.clj
@@ -3,6 +3,7 @@
             [clj-time.core :as t]
             [clj-time.format :as f]
             [me.raynes.cegdown :as md]
+            [blog.author :as author]
             [clojure.string :as str]))
 
 (defn- parse-date [filename]
@@ -35,11 +36,16 @@
                       (f/with-locale (java.util.Locale. "ENGLISH")))]
     (f/unparse formatter date)))
 
+(defn author-link [id]
+  (let [full-name (author/full-name id)
+        twitter-url (author/twitter-url id)]
+    (str "<a href=\"" twitter-url "\">" full-name "</a>")))
+
 (defn to-markdown [layout post]
   (let [human-date (->> post :date format-date)
         rendered (md/to-html (post :body) pegdown-options)
         body (->> rendered
                   (str "<header><a href=\"" (post :url) "\">" (post :title) "</a></header>")
-                  (str "<span class=\"date\">" human-date "</span>\n\n"))
+                  (str "<span class=\"meta\">by " (author-link (post :author)) " on <span class=\"date\">" human-date "</span></span>\n\n"))
         wrapped-body (str "<div class=\"post\">" body "</div>")]
     (fn [req] (layout req wrapped-body))))
